cbe8862cfef4dac9667bec5f63e7e115be41fe63
JossM <josselin.mahe@gmail.com>
2021-05-02 17:52:39 +0200

    :pencil: Add return type in docs

--- backend/api/task.py
+++ backend/api/task.py
@@ -9,11 +9,11 @@ def add_tasks_resources(app: FastAPI):
     """Adds all resources for /tasks resources"""
     route = "/all_tasks"
 
-    @app.get(route)
+    @app.get(route, response_model=List[Task])
     def get() -> List[Task]:  # todo: add pagination
         return list(get_all_tasks())
 
-    @app.post(route)
+    @app.post(route, response_model=str)
     def post(all_tasks: List[Task]) -> str:
         save_all_tasks(all_tasks)
         return f"Successfully registered {len(all_tasks)} task(s)"
