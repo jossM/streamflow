62adf3321ac4aea26ec36e5ac6362eceb97203ea
JossM <josselin.mahe@gmail.com>
2021-08-10 19:15:51 +0200

    :white_check_mark: Add tests

--- backend/tests/api/test_tasks.py
+++ backend/tests/api/test_tasks.py
@@ -31,6 +31,28 @@ def test_put_valid_object_in_empty_dag(scan_table_mock, mock_task_lock, mock_upd
     ))
 
 
+def test_put_task_outside_of_dag(scan_table_mock, mock_task_lock, mock_update_db: AsyncMock):
+    scan_table_mock.return_value = make_scan_table_response(results=[])
+    task = make_task(id=f'other_{dag}/task')
+    put_response = client.put(ROUTE, TasksChange.construct(dags=[dag], tasks=[task]).json(exclude_unset=True))
+    assert put_response.status_code == 422, put_response.json()
+    assert mock_task_lock.mock_calls == []
+    mock_update_db.assert_not_awaited()
+
+
+def test_put_update_tasks_in_dag(scan_table_mock, mock_task_lock, mock_update_db: AsyncMock):
+    initial_task_id, new_task_id = (f"{dag}/initial_task", f"{dag}/new_task")
+    scan_table_mock.return_value = make_scan_table_response(results=[make_task_db(id=f"{dag}/initial_task")])
+    new_task = make_task(id=new_task_id)
+    put_response = client.put(ROUTE, TasksChange(dags=[dag], tasks=[new_task]).json(exclude_unset=True))
+    assert put_response.status_code == 200, put_response.json()
+    assert mock_task_lock.mock_calls == EXPECT_MOCK_CALLS
+    mock_update_db.assert_awaited_once_with(DbTasksChange(
+        ids_to_remove={initial_task_id},
+        tasks_to_update=[DbTask(**new_task.dict(exclude_unset=True))]
+    ))
+
+
 def test_put_delete_dag(scan_table_mock, mock_task_lock, mock_update_db: AsyncMock):
     existing_task = make_task_db(id=f"{dag}/task_id")
     scan_table_mock.return_value = make_scan_table_response(results=[existing_task])
@@ -41,6 +63,14 @@ def test_put_delete_dag(scan_table_mock, mock_task_lock, mock_update_db: AsyncMo
         ids_to_remove={existing_task.id},
         tasks_to_update=[]
     ))
+
+
+def test_put_delete_non_existent_dag(scan_table_mock, mock_task_lock, mock_update_db: AsyncMock):
+    scan_table_mock.return_value = make_scan_table_response(results=[])
+    put_response = client.put(ROUTE, TasksChange.construct(dags=["non_existent_dag"], tasks=[]).json(exclude_unset=True))
+    assert put_response.status_code == 200, put_response.json()
+    assert mock_task_lock.mock_calls == EXPECT_MOCK_CALLS
+    mock_update_db.assert_not_awaited()
 
 
 def test_delete_tasks_that_has_downstream(scan_table_mock, mock_task_lock, mock_update_db: AsyncMock):
